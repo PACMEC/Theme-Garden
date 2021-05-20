969b4be660b254c8a381eaa750f127cfd7c95149
FelipheGomez <feliphegomez@gmail.com>
2021-05-20 01:16:42 -0500

    Up

---
+++ template-parts/content/pages-single.php
@@ -0,0 +1,111 @@
+
+<div id="page-header" class="content-contrast">
+        <div class="page-title-container">
+            <div class="background-overlay"></div>
+            <div class="container centered-container">
+                <div class="centered-inner-container">
+                    <h1 class="page-title">Promo Box</h1>
+                    <hr class="separator small-separator">
+                    <div class="heading-caption">Shortcode for Customization</div>
+                </div>
+            </div>
+        </div>
+        <div class="breadcrumb-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="breadcrumb">
+                            <a class="breadcrumb-item" href="#">Home</a>
+                            <a class="breadcrumb-item" href="#">Mega Menu</a>
+                            <a class="breadcrumb-item" href="#">Element</a>
+                            <span class="breadcrumb-item active">Promo Box</span>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section border-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="promo-box full-width">
+                        <div class="promo-content">
+                            <h4>Example Promo Box with Block Size</h4>
+                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim
+                                veniam exercitation.</p>
+                        </div>
+                        <div class="promo-action">
+                            <a href="#" class="btn gray btn-radius">Purchase Now</a>
+                        </div>
+                    </div>
+
+                    <div class="promo-box blue promo-contrast full-width">
+                        <div class="promo-content">
+                            <h4>Example Promo Box with Block Size</h4>
+                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim
+                                veniam exercitation.</p>
+                        </div>
+                        <div class="promo-action">
+                            <a href="#" class="btn btn-outline white btn-radius">Purchase Now</a>
+                        </div>
+                    </div>
+
+                    <div class="promo-box green promo-contrast full-width">
+                        <div class="promo-content">
+                            <h4>Example Promo Box with Block Size</h4>
+                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim
+                                veniam exercitation.</p>
+                        </div>
+                        <div class="promo-action">
+                            <a href="#" class="btn btn-outline white btn-radius">Purchase Now</a>
+                        </div>
+                    </div>
+
+                    <div class="promo-box yellow promo-contrast full-width">
+                        <div class="promo-content">
+                            <h4>Example Promo Box with Block Size</h4>
+                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim
+                                veniam exercitation.</p>
+                        </div>
+                        <div class="promo-action">
+                            <a href="#" class="btn btn-outline white btn-radius">Purchase Now</a>
+                        </div>
+                    </div>
+
+                    <div class="promo-box red promo-contrast full-width">
+                        <div class="promo-content">
+                            <h4>Example Promo Box with Block Size</h4>
+                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim
+                                veniam exercitation.</p>
+                        </div>
+                        <div class="promo-action">
+                            <a href="#" class="btn btn-outline white btn-radius">Purchase Now</a>
+                        </div>
+                    </div>
+
+                    <div class="promo-box gray promo-contrast full-width">
+                        <div class="promo-content">
+                            <h4>Example Promo Box with Block Size</h4>
+                            <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore ut enim ad minim
+                                veniam exercitation.</p>
+                        </div>
+                        <div class="promo-action">
+                            <a href="#" class="btn btn-outline white btn-radius">Purchase Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
